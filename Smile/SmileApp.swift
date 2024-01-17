e0c5ed1abe8d55611a0b3bf0a84903ec567558d1
Gridenko Maks <Maksgridenko@ya.ru>
2024-01-17 18:46:44 +0300

    Initial Commit

---
+++ Smile/SmileApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmileApp.swift
+//  Smile
+//
+//  Created by Maxim Gridenko on 17.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct SmileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
